3782212b629c6e22fab61cd27b5371ab20524c89
Pixel-Code-Hub206 <mayanklan206@gmail.com>
2025-09-25 01:04:52 +0530

    Expanded the choose location page by adding more options

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,15 +11,38 @@ class ChooseLocation extends StatefulWidget {
 class _ChooseLocationState extends State<ChooseLocation> {
 
  List<WorldTime> locations = [
-   WorldTime(location: 'Kolkata', time: "", flag: 'india.png', url: 'Asia%2FKolkata', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'London', time: "", flag: 'uk.png', url: 'Europe%2FLondon', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'Athens', time: "", flag: 'greece.png', url: 'Europe%2FBerlin', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'Cairo', time: "", flag: 'egypt.png', url: 'Africa%2FCairo', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'Nairobi', time: "", flag: 'kenya.png', url: 'Africa%2FNairobi', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'Chicago', time: "", flag: 'usa.png', url: 'America%2FChicago', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'New York', time: "", flag: 'usa.png', url: 'America%2FNew_York', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'South Korea', time: "", flag: 'south_korea.png', url: 'Asia%2FSeoul', isDayTime: true),    //Parameters to get time and flag data for the locations
-   WorldTime(location: 'Jakarta', time: "", flag: 'indonesia.png', url: 'Asia%2FJakarta', isDayTime: true),    //Parameters to get time and flag data for the locations
+   //Parameters to get time and flag data for the locations
+   WorldTime(location: 'Kolkata', time: "", flag: 'india.png', url: 'Asia%2FKolkata', isDayTime: true),
+   WorldTime(location: 'London', time: "", flag: 'uk.png', url: 'Europe%2FLondon', isDayTime: true),
+   WorldTime(location: 'New York', time: "", flag: 'usa.png', url: 'America%2FNew_York', isDayTime: true),
+   WorldTime(location: 'Los Angeles', time: "", flag: 'usa.png', url: 'America%2FLos_Angeles', isDayTime: true),
+   WorldTime(location: 'Toronto', time: "", flag: 'canada.png', url: 'America%2FToronto', isDayTime: true),
+   WorldTime(location: 'Vancouver', time: "", flag: 'canada.png', url: 'America%2FVancouver', isDayTime: true),
+   WorldTime(location: 'Berlin', time: "", flag: 'germany.png', url: 'Europe%2FBerlin', isDayTime: true),
+   WorldTime(location: 'Paris', time: "", flag: 'france.png', url: 'Europe%2FParis', isDayTime: true),
+   WorldTime(location: 'Madrid', time: "", flag: 'spain.png', url: 'Europe%2FMadrid', isDayTime: true),
+   WorldTime(location: 'Rome', time: "", flag: 'italy.png', url: 'Europe%2FRome', isDayTime: true),
+   WorldTime(location: 'Zurich', time: "", flag: 'switzerland.png', url: 'Europe%2FZurich', isDayTime: true),
+   WorldTime(location: 'Helsinki', time: "", flag: 'finland.png', url: 'Europe%2FHelsinki', isDayTime: true),
+   WorldTime(location: 'Moscow', time: "", flag: 'russia.png', url: 'Europe%2FMoscow', isDayTime: true),
+   WorldTime(location: 'Tokyo', time: "", flag: 'japan.png', url: 'Asia%2FTokyo', isDayTime: true),
+   WorldTime(location: 'Seoul', time: "", flag: 'southkorea.png', url: 'Asia%2FSeoul', isDayTime: true),
+   WorldTime(location: 'Singapore', time: "", flag: 'singapore.png', url: 'Asia%2FSingapore', isDayTime: true),
+   WorldTime(location: 'Dubai', time: "", flag: 'uae.png', url: 'Asia%2FDubai', isDayTime: true),
+   WorldTime(location: 'Sydney', time: "", flag: 'australia.png', url: 'Australia%2FSydney', isDayTime: true),
+   WorldTime(location: 'Mexico City', time: "", flag: 'mexico.png', url: 'America%2FMexico_City', isDayTime: true),
+   WorldTime(location: 'São Paulo', time: "", flag: 'brazil.png', url: 'America%2FSao_Paulo', isDayTime: true),
+   WorldTime(location: 'Cape Town', time: "", flag: 'southafrica.png', url: 'Africa%2FJohannesburg', isDayTime: true),
+   WorldTime(location: 'Cairo', time: "", flag: 'egypt.png', url: 'Africa%2FCairo', isDayTime: true),
+   WorldTime(location: 'Istanbul', time: "", flag: 'turkey.png', url: 'Europe%2FIstanbul', isDayTime: true),
+   WorldTime(location: 'Bangkok', time: "", flag: 'thailand.png', url: 'Asia%2FBangkok', isDayTime: true),
+   WorldTime(location: 'Stockholm', time: "", flag: 'sweden.png', url: 'Europe%2FStockholm', isDayTime: true),
+   WorldTime(location: 'Amsterdam', time: "", flag: 'netherlands.png', url: 'Europe%2FAmsterdam', isDayTime: true),
+   WorldTime(location: 'Beijing', time: "", flag: 'china.png', url: 'Asia%2FShanghai', isDayTime: true),
+   WorldTime(location: 'Wellington', time: "", flag: 'newzealand.png', url: 'Pacific%2FAuckland', isDayTime: true),
+   WorldTime(location: 'Nairobi', time: "", flag: 'kenya.png', url: 'Africa%2FNairobi', isDayTime: true),
+   WorldTime(location: 'Chicago', time: "", flag: 'usa.png', url: 'America%2FChicago', isDayTime: true),
+   WorldTime(location: 'Jakarta', time: "", flag: 'indonesia.png', url: 'Asia%2FJakarta', isDayTime: true),
   ];
 
  void updateTime(index) async{
